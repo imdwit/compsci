ddb11b41ed4f7950ae6501dad7160d83dbdf1907
imdwit <dwightdoane@me.com>
2015-01-18 21:42:33 -0500

    added C

---
+++ helloworld/C/helloworld/helloworld/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by Dwight Doane on 1/18/15.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
